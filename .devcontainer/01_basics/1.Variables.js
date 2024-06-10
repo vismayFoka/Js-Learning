c39645b713718d610b24a984c9d4d4eb09e78b21
vismayFoka <106068397+vismayFoka@users.noreply.github.com>
2024-06-10 16:44:53 +0000

    Added Variables.js file

---
+++ .devcontainer/01_basics/1.Variables.js
@@ -0,0 +1,13 @@
+const accountId = 12
+let accountName = "Vismay"
+var accountSalary = 5000
+accountCity = "Mumbai"
+let accountState
+
+console.table([accountId,accountName,accountSalary,accountState])
+
+accountName ="John"
+accountSalary = 5500
+accountCity = "Pune"
+
+console.table([accountId,accountName,accountSalary,accountState])
