c2f415d50d1cff31570385df08b7b6c79c1c3d99
vismayFoka <106068397+vismayFoka@users.noreply.github.com>
2024-06-11 06:57:08 +0000

    renamed folders

---
+++ 01_basics/2_DataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS Code as a newer version
+
+let name="Vismay"
+let age = 18
+let state = null
+
+//number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// undefined => The undefined property indicates that a variable has not been assigned a value, or not declared at all.
+// null => standalone value,Not a datatype
+// Symbol
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
